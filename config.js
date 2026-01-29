42f98b03b74fbd2eb4db3335448fe9d68260a39f
lexmpineda-dot <lexmpineda@gmail.com>
2026-01-30 00:44:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Ivy, my baby",
+    valentineName: "Ivy",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
